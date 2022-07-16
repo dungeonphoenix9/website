32b334fd52f1a5f1b2db3b0531edc57fa4e94859
dungeonphoenix9 <87551809+dungeonphoenix9@users.noreply.github.com>
2022-07-16 17:46:25 +0100

    e

--- apod.js
+++ apod.js
@@ -1,12 +1,12 @@
 function contentLoader() {
     sendApiReq();
-};
+}
 
 async function sendApiReq() {
     let res = await fetch('https://api.nasa.gov/planetary/apod?api_key=LO6kQ6p7PbYP8Q2OBHcmqsH7WYqdZL6RAvB5SN10');
     let data = await res.json();
     useApiData(data);
-};
+}
 
 function useApiData(data) {
     document.querySelector("#title").innerHTML += data.title;
