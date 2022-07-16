c0d3ba7077c30ea074279951ce4630ea03071395
dungeonphoenix9 <87551809+dungeonphoenix9@users.noreply.github.com>
2022-07-16 15:44:44 +0100

    added apod

---
+++ apod.js
@@ -0,0 +1,16 @@
+function contentLoader() {
+    sendApiReq();
+};
+
+async function sendApiReq() {
+    let APIKEY = 'LO6kQ6p7PbYP8Q2OBHcmqsH7WYqdZL6RAvB5SN10'
+    let res = await fetch('https://api.nasa.gov/planetary/apod?api_key=${APIKEY}')
+    let data = await res.json();
+    useApiData(data);
+};
+
+function useApiData(data) {
+    document.querySelector("#title").innerHTML += data.title;
+    document.querySelector("#content").innerHTML += ' <img src="${data.url}" class="main-img" /> <br/>';
+    document.querySelector("#content").innerHTML += data.explanation;
+}
